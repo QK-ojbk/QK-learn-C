03f4ff56a520522790b58f838614afd78f7a4ae6
QK-ojbk <102276407+QK-ojbk@users.noreply.github.com>
2022-03-24 13:31:52 +0800

    learn-3-24-2
    hello 草泥马

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+//包含一个stdio.h的文件，头文件
+//std-标准，i-input，o-output
 #include <stdlib.h>
 
-int main()
+int main()//main前面的int 便是main函数调用返回一个整型值
 {
-    printf("Hello world!\n");
+    printf("Hello world!草泥马\n");
+    //此处printf函数是库函数(打印函数)：print+function
+    //"\n"表示换行
     return 0;
 }
+//格式：
+//#include<stdio.h>(添加各种头文件)
+//int main()
+//{
+//   程序（想实现的内容）
+//   return 0；
+//}
